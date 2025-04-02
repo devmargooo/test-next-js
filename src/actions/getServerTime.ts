9b07c84fb9b9bef3d76d38e3ba20188628f74b0b
devmargooo <devmargooo@gmail.com>
2025-04-02 20:43:25 +0300

    edit api

--- src/actions/getServerTime.ts
+++ src/actions/getServerTime.ts
@@ -1,7 +1,7 @@
-"use server";
+'use server';
 export async function getServerTime() {
-  return await fetch("http://localhost:3001/hello", {
-    method: "GET",
-    cache: "no-store",
+  return await fetch('http://82.202.142.18:3001/hello', {
+    method: 'GET',
+    cache: 'no-store',
   }).then((res) => res.text());
 }
--- src/components/SuspendedDate.tsx
+++ src/components/SuspendedDate.tsx
@@ -1,8 +1,10 @@
 export const SuspendedDate = async () => {
-  const date = await fetch("http://localhost:3001/hello", {
-    method: "GET",
-    cache: "no-store",
-  }).then((res) => res.text());
+  const date = await fetch('http://82.202.142.18:3001/hello', {
+    method: 'GET',
+    cache: 'no-store',
+  })
+    .then((res) => res.text())
+    .catch(() => void 0);
 
   return <p>Get date from server: {date}</p>;
 };
